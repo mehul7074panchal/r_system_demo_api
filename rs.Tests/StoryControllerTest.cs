28b0899e476403ca289bbc4198c833eebe25f5ea
Mehul Panchal <mehul@bugsfreeinnovation.com>
2024-07-29 16:03:32 +0530

    Initial commit

---
+++ rs.Tests/StoryControllerTest.cs
@@ -0,0 +1,56 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace rs.Tests
+{
+    [TestFixture]
+    public class StoryControllerTest
+	{
+        private HttpClient? _client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var appFactory = new WebApplicationFactory<Program>();
+            _client = appFactory.CreateClient();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _client?.Dispose();
+        }
+
+        [Test]
+        [TestCase(0,0)]
+        [TestCase(1,0)]
+        [TestCase(-1,-1)]
+        [TestCase(20, 200)]
+        [TestCase(30, 500)]
+        public async Task GetStory_ReturnBadRequest_WhenInavlidPageOrPageSize(int page, int pageSzie)
+        {
+           
+            // Act
+            var response = await _client.GetAsync($"/api/story/GetStory/{page}/{pageSzie}");
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.BadRequest));
+        }
+
+        [Test]
+        [TestCase(1, 10)]
+        [TestCase(1, 50)]
+        [TestCase(2, 20)]
+        public async Task GetStory_ReturnOk_WhenvalidData(int page, int pageSzie)
+        {
+
+            // Act
+            var response = await _client.GetAsync($"/api/story/GetStory/{page}/{pageSzie}");
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+        }
+    }
+}
+
---
+++ rs.Tests/StoryRepositoryTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using Newtonsoft.Json.Linq;
+using rs.Helper;
+using rs.model;
+using rs.Repository;
+
+namespace rs.Tests
+{
+	public class StoryRepositoryTests
+	{
+        private Mock<IMemoryCache> _mockCache;
+        private Mock<IHttpHelerClient> _mokHttp;
+        private StoryRepository _story;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockCache = new Mock<IMemoryCache>();
+            var ids = new List<int>();
+            var stories = new List<Story>();
+            for (int i = 1; i <= 200; i++)
+            {
+                ids.Add(i);
+                stories.Add(new Story { Id = i, Title = "Story-" + i, URL = "Story-" + i+".com" });
+            }
+            object cacheEntry;
+            _mockCache.Setup(m => m.TryGetValue(It.IsAny<object>(), out cacheEntry))
+                      .Callback(new TryGetValueCallback<object, object>((object key, out object value) => value = ids))
+                      .Returns(true);
+            _mokHttp = new Mock<IHttpHelerClient>();
+            _mokHttp.Setup(m => m.HttpGetRequest<Story>(It.IsAny<string>())).Returns(Task.FromResult(new Story { Id = 1, Title = "Story-1", URL = "Story-1.com" }));
+            _story = new StoryRepository(_mockCache.Object, _mokHttp.Object);
+        }
+        private delegate void TryGetValueCallback<TKey, TValue>(TKey key, out TValue value);
+
+        [Test]
+        [TestCase(-1, -1)]
+        [TestCase(20, 200)]
+        [TestCase(30, 500)]
+        public void GetStoriesAsync_WithInvalidPageOrPageSize_ThrowsArgumentExceptionn(int page, int pageSize)
+        {
+            Assert.That(() => _story.GetStoriesAsync(page, pageSize), Throws.ArgumentException);
+        }
+
+        [Test]
+        [TestCase(1, 10)]
+        [TestCase(2, 10)]
+        public async Task GetStoriesAsync_WithInvalidPageOrPageSize_GetListOfStory(int page, int pageSize)
+        {
+            var list = await _story.GetStoriesAsync(page, pageSize);
+            Assert.That(() => list, !Is.Null);
+        }
+    }
+}
+
